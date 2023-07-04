81120ad071ae8978cab3ad7297aecf0dcda70224
cookieMonsterDev <mykhailo.toporkov@gmail.com>
2023-07-04 17:04:57 +0300

    feat: add cors

--- nest-rest-mongodb-mongoose/src/main.ts
+++ nest-rest-mongodb-mongoose/src/main.ts
@@ -6,6 +6,12 @@ config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    allowedHeaders: 'Content-Type, Accept, Authorization',
+    credentials: true,
+  });
   await app.listen(6565);
 }
 bootstrap();
